3375ac3d1d3c0f58af1097477b0102dba5296524
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-07-27 10:46:09 +0530

    fs and os modules testing

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-console.log("first");
-
 console.log("Hello Node Js");
+
+var fs = require("fs");
+var os = require("os");
+
+var user = os.userInfo();
+console.log(user);
+console.log(user.username);
+
+// os kya kya kar sakata hai
+// console.log(os);
+
+
+// file system
+// 1.file path
+// 2.data
+// 3.callback function
+
+// fs.appendFile('greeting.text', 'Hi' + user.username + '!\n', () => {
+//     console.log("file is created");
+// });
